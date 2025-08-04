3b428497bfdfabe441a4f375477a2a6eee4f3bd9
VishalSagar7 <155461251+VishalSagar7@users.noreply.github.com>
2025-08-04 22:06:11 +0530

    commit four

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import { LoginValidationSchema } from '../Validations/LoginPageValidation';
-import LoginFormBgImg from "../assets/LoginFormBgImg.png";
+import LoginFormBgImg from "../assets/LoginFormBgimg.png";
 import InputField from '../components/commonComponent/InputField';
 import inputFieldCss from '../components/utils/inputFieldCss';
 import { useNavigate } from 'react-router-dom';
