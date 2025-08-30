53b79ef3eec350079607536c45c0bc43b51ee236
VishalSagar7 <155461251+VishalSagar7@users.noreply.github.com>
2025-08-30 21:48:28 +0530

    added the rupee sign

--- src/cards/FullHomePaintingCard.jsx
+++ src/cards/FullHomePaintingCard.jsx
@@ -17,7 +17,7 @@ const FullHomePaintingCard = ({ item, idx }) => {
                         <div key={idx} className='h-[220px] md:h-[200px] flex gap-2 border-b-[0.1px] border-gray-300 pt-4  '>
                             <div className=' w-[70%] md:w-[65%] text-black '>
                                 <h2 className='font-semibold text-lg'>{ type?.heading }</h2>
-                                <p className='font-semibold'>Starts at { type?.price }</p>
+                                <p className='font-semibold'>Starts at ₹{ type?.price }</p>
                                 <hr className='my-4 border-dashed border-gray-300' />
                                 <p className='text-gray-700 text-sm'>{ type?.description }</p>
                                 <p className='text-[#8a0136] cursor-pointer my-2 font-semibold text-md'>View details & estimate</p>
